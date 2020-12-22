dc1c959fcfa6abd403d16991551daef04aedd108
李韻晟 <dreak603@gmail.com>
2020-12-22 23:01:00 +0800

    fix: fix typo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from 'u@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from './users/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './config/orm.config';
