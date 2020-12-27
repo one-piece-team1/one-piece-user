41de20e608378997a44492b53fdd79c16a286d41
李韻晟 <dreak603@gmail.com>
2020-12-27 16:19:39 +0800

    Chore: update config db name and port

--- config.ts
+++ config.ts
@@ -100,12 +100,12 @@ const configs = {
 
     DB_SETTINGS: {
       host: process.env.DBHOST || 'localhost',
-      port: process.env.DBPORT || 5432,
+      port: process.env.DBPORT || 5434,
       username: process.env.DBUSERNAME || 'postgres',
       password: process.env.DBPASSWORD || '123',
       database: process.env.DBDATABASE || 'lib',
       schema: process.env.DBSCHEMA || 'public',
-      userTable: process.env.DBUSERTABLE || 'user',
+      userTable: process.env.DBUSERTABLE || 'onepiece',
     },
 
     REDIS_URL: process.env.REDIS_URL || "redis://127.0.0.1:6382",
