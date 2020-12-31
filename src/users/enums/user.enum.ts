a36d69263560e653ade7850630a57ccff5b61a92
李韻晟 <dreak603@gmail.com>
2021-01-01 00:22:29 +0800

    chore: update temp user entity define

---
+++ src/users/enums/user.enum.ts
@@ -0,0 +1,12 @@
+export enum EUserRole {
+  'TRIAL' = 'trial',
+  'USER' = 'user',
+  'VIP1' = 'vip1',
+  'VIP2' = 'vip2',
+  'ADMIN' = 'admin',
+}
+
+export enum EUserGender {
+  'MALE' = 'male',
+  'FEMALE' = 'female',
+}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -11,6 +11,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import * as EUser from './enums';
 
 @Entity()
 @Unique(['username', 'email'])
@@ -18,8 +19,11 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', default: 'user', nullable: false, insert: false })
-  role: string;
+  /**
+   * @todo decide how to setup user role and which default yet
+   */
+  @Column({ type: 'enum', enum: EUser.EUserRole, default: EUser.EUserRole.USER, nullable: false, insert: false })
+  role: EUser.EUserRole;
 
   @Column({ type: 'varchar', nullable: false })
   @Index({ unique: true })
@@ -35,8 +39,27 @@ export class User extends BaseEntity {
   @Column({ nullable: false })
   salt: string;
 
+  // represent if user is soft deleted or not, true means not deleted
   @Column({ type: 'boolean', default: true })
   status: boolean;
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  followerCount: number;
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  followingCount: number;
+
+  @Column({ type: 'enum', enum: EUser.EUserGender, nullable: true })
+  gender?: EUser.EUserGender;
+
+  @Column({ type: 'int', nullable: true })
+  age?: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  desc?: string;
+
+  @Column({ type: 'timestamp', nullable: false })
+  expiredDate: number;
 
   @CreateDateColumn({ nullable: false })
   createdAt: Date;
